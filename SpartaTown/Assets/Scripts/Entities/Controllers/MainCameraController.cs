cccab539ebdbea123b0cbaeb2309d499674c249d
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-10-11 11:51:21 +0900

    add TMP & Font

--- SpartaTown/Assets/Scripts/Entities/Controllers/MainCameraController.cs
+++ SpartaTown/Assets/Scripts/Entities/Controllers/MainCameraController.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class MainCameraController : MonoBehaviour
 {
-    [SerializeField] Transform player;
     [SerializeField] float smoothing = 0.2f;
 
     private void FixedUpdate()
     {
-        Vector3 targetPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+        if (GameManager.Instance.player.transform == null) return;
+        Vector3 targetPos = new Vector3(GameManager.Instance.player.transform.position.x, GameManager.Instance.player.transform.position.y, transform.position.z);
         transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
     }
 }

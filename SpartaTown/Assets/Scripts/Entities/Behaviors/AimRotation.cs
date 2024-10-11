48c98b117c68d23d57268e86147a40eaff11c6bc
Sungchan Ahn <146975762+sungchanahn@users.noreply.github.com>
2024-10-11 11:49:48 +0900

    fix 캐릭터가 처음 바라보고 있는 방향에 따른 AimRotaion 수정
    AimRotaion을 만들고 왼쪽을 바라보고 있는 캐릭터와 오른쪽을 바라보고 있는 캐릭터로 나눠 Script를 작성

---
+++ SpartaTown/Assets/Scripts/Entities/Behaviors/AimRotation.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class AimRotation : MonoBehaviour
+{
+    protected TopDownController controller;
+
+    protected void Awake()
+    {
+        controller = GetComponent<TopDownController>();
+    }
+
+    protected void Start()
+    {
+        controller.OnLookEvent += OnAim;
+    }
+
+    protected void OnAim(Vector2 direction)
+    {
+        RotateCharacter(direction);
+    }
+
+    protected virtual void RotateCharacter(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+    }
+}
---
+++ SpartaTown/Assets/Scripts/Entities/Behaviors/LookingLeftCharacterAimRotation.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class LookingLeftCharacterAimRotation : AimRotation
+{
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected override void RotateCharacter(Vector2 direction)
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        characterRenderer.flipX = !(Mathf.Abs(rotZ) > 90f);
+    }
+}
